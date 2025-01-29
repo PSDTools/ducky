d552411e3fbcf777112d105c3a1ff4abc3eb7ec2
Eli <88557639+lishaduck@users.noreply.github.com>
2025-01-29 12:30:58 -0600

    chore: format

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+/** @type {import('prettier').Config} */
+export default {
+  trailingComma: "all",
+  singleQuote: false,
+  semi: true,
+  tabWidth: 2,
+  useTabs: false,
+  endOfLine: "lf",
+  experimentalTernaries: true,
+};
