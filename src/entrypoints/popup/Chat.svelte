2b1fed6034f84fe324c7f2cf6809d3bbdd5c5e99
iiAzui <rhys.harden@gmail.com>
2025-01-30 14:15:45 -0600

    wip: init chat
    basic chat features,
    NO RESPONSE

--- src/entrypoints/popup/Chat.svelte
+++ src/entrypoints/popup/Chat.svelte
@@ -1,7 +1,22 @@
 <script lang="ts">
   import Wrapper from "./Wrapper.svelte";
+
+  var text = $state<string>();
 </script>
 
 <Wrapper pageTitle="Ducky Chat">
-  <form></form>
+  <form
+    onsubmit={() => {
+      alert(text);
+    }}
+  >
+    <input bind:value={text} type="text" class="textbox" id="textbox" />
+    <input type="submit" value="submitText" />
+  </form>
 </Wrapper>
+
+<style>
+  .textbox {
+    color: black;
+  }
+</style>
