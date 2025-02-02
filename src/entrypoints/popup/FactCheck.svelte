cd57a60ace17df70a3f9bc3827561fde20d6b8f3
Eli <88557639+lishaduck@users.noreply.github.com>
2025-02-01 20:48:04 -0600

    feat: better forms
    Like, look at this CSS!
    
    Pretty animations, nice colors, and accessible, semantic HTML.
    And it's all thanks to pure (scoped) CSS.
    Gotta love it.

--- src/entrypoints/popup/FactCheck.svelte
+++ src/entrypoints/popup/FactCheck.svelte
@@ -1,38 +1,32 @@
 <script lang="ts">
+  import RadioButton from "./RadioButton.svelte";
   import Wrapper from "./Wrapper.svelte";
 </script>
 
 <Wrapper pageTitle="Fact Checker">
-  <label class="option">
-    <input type="radio" name="answer" value="A" />
-    <span class="bubble"></span>Use Current Website
-  </label>
-  <label class="option">
-    <input type="radio" name="answer" value="B" />
-    <span class="bubble"></span>Use Other Website
-  </label>
+  <form>
+    <fieldset>
+      <legend>What would you like to fact check?</legend>
+      <div>
+        <div>
+          <RadioButton id="current" name="answer" value="current" />
+          <label for="current" class="option">Use Current Website</label>
+        </div>
+        <div>
+          <RadioButton id="other" name="answer" value="other" />
+          <label for="other" class="option">Use Other Website</label>
+        </div>
+      </div>
+    </fieldset>
+  </form>
 </Wrapper>
 
 <style>
   .option {
-    display: flex;
+    display: inline-block;
     align-items: center;
     margin: 5px 0;
     cursor: pointer;
     font-size: 16px;
-  }
-  .bubble {
-    width: 20px;
-    height: 20px;
-    border-radius: 50%;
-    border: 2px solid #ebde1f;
-    margin-right: 10px;
-    display: inline-block;
-  }
-  input[type="radio"] {
-    display: none;
-  }
-  input[type="radio"]:checked + .bubble {
-    background-color: #ebde1f;
   }
 </style>
---
+++ src/entrypoints/popup/RadioButton.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  interface Props {
+    id: string;
+    name: string;
+    value: string;
+  }
+
+  const props: Props = $props();
+</script>
+
+<input {...props} type="radio" />
+
+<style>
+  input[type="radio"] {
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    border: 2px solid #e2c100;
+    margin-right: 10px;
+    display: inline-block;
+
+    transition:
+      background-color 0.5s,
+      transform 0.2s;
+
+    &:checked {
+      background-color: #f0d500;
+    }
+
+    &:hover {
+      transform: scale(1.05);
+    }
+  }
+</style>
