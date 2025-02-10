d7df075ee8da039446ba6a7dd81e3ee0f8dfe1c9
Eli <88557639+lishaduck@users.noreply.github.com>
2025-02-10 13:06:58 -0600

    fix: better phishing ui

--- src/entrypoints/popup/Phishing.svelte
+++ src/entrypoints/popup/Phishing.svelte
@@ -1,75 +1,32 @@
 <script lang="ts">
   import Wrapper from "./Wrapper.svelte";
+  import { AlertTriangle } from "lucide-svelte";
 </script>
 
 <Wrapper pageTitle="Phishing">
-  <h2>Phishing Alert!</h2>
+  <div class="center-stuff">
+    <AlertTriangle color="#ff0000" size="8rem" />
 
-  <p>
-    That video looks Quackers! It's definitely a fake! Be weary of any
-    attachments or links! Click more info for more info! Quack!
-  </p>
+    <p>This message may be from quacks!</p>
 
-  <div class="open">
-    <a class="chat-button" href="https://chatgpt.com">More Info.</a>
+    <div class="open">
+      <a class="chat-button" href="https://chatgpt.com">
+        How can I spot phishing?
+      </a>
+    </div>
   </div>
-
-  <!-- <form>
-    <fieldset>
-      <Toggle id="phishing" />
-      <label class="toggle-switch" for="phishing">Enable Phishing Detector</label>
-    </fieldset>
-     <fieldset>
-      <Toggle id="site-detection" />
-      <label class="toggle-switch" for="site-detection">
-        Insecure Site Detector
-      </label>
-    </fieldset>
-    <fieldset>
-      <Toggle id="mask" />
-      <label for="mask" class="toggle-switch">PPI Masker</label>
-    </fieldset> 
-  </form> -->
 </Wrapper>
 
 <style>
-  .textbox {
-    color: black;
-    width: 70%;
-    field-sizing: content;
-    font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
-    resize: none;
-
-    border-radius: 1rem;
-  }
-  .submit {
-    all: unset;
-    outline: revert;
-    background-color: #e2c100;
-
-    display: inline-flex;
+  .center-stuff {
+    display: flex;
+    flex-direction: column;
     align-items: center;
     justify-content: center;
-
-    height: 2rem;
-    width: 2rem;
-    border-radius: 0.5rem;
-
-    transition:
-      background-color 0.3s,
-      transform 0.2s;
-
-    &:hover {
-      cursor: pointer;
-      background-color: #f0d500;
-      transform: scale(1.075);
-    }
+    gap: 0.5rem;
+    text-align: center;
   }
-  .form {
-    display: flex;
-    align-items: center;
-    gap: 0.75rem;
-  }
+
   .open {
     display: flex;
     flex-direction: column;
