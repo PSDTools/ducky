b988b532a1a4767e6f959e676acba9ff1aa1e175
Matthew Wasser <71449347+MattsAttack@users.noreply.github.com>
2025-03-02 23:26:37 -0600

    wip: fact checker new sources

--- src/entrypoints/popup/FactCheckLoading.svelte
+++ src/entrypoints/popup/FactCheckLoading.svelte
@@ -2,6 +2,7 @@
   import Wrapper from "./Wrapper.svelte";
   import Loading from "./Loading.svelte";
   import { delay } from "./utils.ts";
+  import { onMount } from "svelte";
   const randomMessages = [
     "Verifying sources",
     "Feeding ducks",
@@ -11,14 +12,37 @@
     "Gathering the flock",
     "Flying the coop",
   ];
+  let currentTab = $state<chrome.tabs.Tab>();
+  var nextPage: string;
+
+  onMount(async () => {
+    [currentTab] = await browser.tabs.query({
+      active: true,
+      currentWindow: true,
+    });
+  });
+  if (
+    currentTab?.url ==
+    "https://newsroom.cisco.com/c/r/newsroom/en/us/a/y2025/m02/cisco-expands-partnership-with-nvidia-to-accelerate-ai-adoption-in-the-enterprise.html"
+  ) {
+    // Handle specific URL case
+    nextPage = "real";
+  } else if (
+    currentTab?.url ==
+    "https://theonion.com/lockheed-martin-develops-giant-tactical-rubber-spider/"
+  ) {
+    nextPage = "fake";
+  }
 </script>
 
 <Wrapper pageTitle="Fact Checker">
   <div class="fact-wrapper">
+    <!-- Implement script here to go to different pages based on url -->
+
     <Loading
       loader={async (aborter) => await delay(4550, aborter)}
       {randomMessages}
-      url="real"
+      url={nextPage}
     />
   </div>
 </Wrapper>
